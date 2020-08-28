fdaec33395f4819eefe7da0ca540651dc910a3d3
Mágila Morgana S. Ferreira <magila.ferreira@etec.sp.gov.br>
2020-08-27 23:02:37 -0300

    configuração da base do layout

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+// Importação das dependências
 const express = require('express')
+const path = require('path')
 
+// Inicializar o express
 const app = express()
 
+// View engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'src', 'app', 'views'))
+
+// Pasta estática
+app.use(express.static(path.join(__dirname, 'src', 'public')))
+
+// Rotas
 app.get("/", (req, res) => {
-  res.send("Olá mundo!")
+  res.render('formulario_imagem')
 })
 
+// Ouvindo a rota
 app.listen(3000)
